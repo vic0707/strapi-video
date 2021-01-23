1e4340dae01dafad7ca7f3f27a1e9159f6d5c80d
Victor Macias Castillo <v.maciascastillo@gmail.com>
2021-01-23 18:22:19 +0100

    Added strapi backend + mongo atlass connection

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.fx0yg.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-video'),
+        username: env('DATABASE_USERNAME', 'strapi_video'),
+        password: env('DATABASE_PASSWORD', '66AYbF2YqTKiCoDI'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1a8af64455d707142024e38a889aeaa3'),
+    },
+  },
+});
